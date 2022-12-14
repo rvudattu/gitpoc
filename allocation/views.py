84920b2716a3ce42665de3416852d6398d39a591
Raj Vihari V <93786419+rvudattu@users.noreply.github.com>
2022-12-14 22:52:31 +0530

    Update views.py

--- allocation/views.py
+++ allocation/views.py
@@ -21,7 +21,7 @@ def count_cur_sim(request):
         conn.close()
         return JsonResponse({"status": "success", "data": res})
         '''
-        return JsonResponse({"status": "success", "data": "Success"})
+        return JsonResponse({"status": "success", "data": "Response from main branch"})
     except Exception as e:
         return JsonResponse({"status": "failed", "msg": str(e)})
 
