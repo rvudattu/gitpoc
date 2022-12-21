1a0095b0a320777fd03f9a1287fd55011bf19672
pgarikap <121164019+pgarikap@users.noreply.github.com>
2022-12-21 19:49:39 +0530

    Update views.py

--- allocation/views.py
+++ allocation/views.py
@@ -25,13 +25,13 @@ def count_cur_sim(request):
     except Exception as e:
         return JsonResponse({"status": "failed", "msg": str(e)})
     
- @api_view(['POST'])
-def test_api(request):
-    try:
-        return JsonResponse({"status": "success", "data": "Created by rvudattu user."})
-#         return JsonResponse({"status": "success", "data": "Created by Pgarikap user for testing.."})
-    except Exception as e:
-        return JsonResponse({"status": "failed", "msg": str(e)})
+#  @api_view(['POST'])
+# def test_api(request):
+#     try:
+#         return JsonResponse({"status": "success", "data": "Created by rvudattu user."})
+# #         return JsonResponse({"status": "success", "data": "Created by Pgarikap user for testing.."})
+#     except Exception as e:
+#         return JsonResponse({"status": "failed", "msg": str(e)})
 
 
 @api_view(['GET'])
