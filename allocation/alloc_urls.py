12a550f7a1e6b1c0d03ffb7b16d858c6954fdc32
pgarikap <121164019+pgarikap@users.noreply.github.com>
2022-12-22 17:52:27 +0530

    Update alloc_urls.py

--- allocation/alloc_urls.py
+++ allocation/alloc_urls.py
@@ -2,7 +2,7 @@ import django.conf.urls
 from django.urls import path, include
 from django.urls import path
 
-from allocation.views import count_cur_sim, getresponse
+from allocation.views import count_cur_sim, getresponse, test_api_new
 
 # urlpatterns =[
 # django.conf.urls.url(r'^count_cur_sim', count_cur_sim, name="To get count of rows.") ,
@@ -11,5 +11,5 @@ from allocation.views import count_cur_sim, getresponse
 urlpatterns =[
     path(r'count_cur_sim', count_cur_sim, name="To get count of rows."),
     path('getresponse', getresponse, name="To get response."),
-    path('test_api', test_api, name="To api in dev.")
+    path('test_api_new', test_api_new, name="To api in dev.")
 ]
